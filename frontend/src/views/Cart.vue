e542257dd1352320b63f5afffa79b08b15da5688
Harliin <56713048+Harliin@users.noreply.github.com>
2021-02-11 10:04:39 +0100

    wip

--- frontend/src/views/Cart.vue
+++ frontend/src/views/Cart.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="cart">
-      <h1>Cart</h1>
+      <h1>Your Cart</h1>
+      <div class="items">
+        <header>
+          <h4>ITEMS</h4>
+          <span class="line"></span>
+        </header>
+      </div>
+      <div class="delivery"></div>
+      <div class="payment-details"></div>
   </div>
 </template>
 
@@ -10,6 +18,43 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.cart{
+  width: 100%;
+  height: 90vh;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 20% 80%;
+  justify-content: center;
+  align-items: center;
 
+  .line{
+    width: 268px;
+    height: 1px;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+  }
+  .items{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    grid-column: 1/2;
+    grid-row: 2/3;
+
+    header{
+      display: flex;
+      flex-direction: row;
+    }
+
+  }
+  .delivery{
+
+  }
+  .payment-details{
+
+  }
+
+}
 </style>
